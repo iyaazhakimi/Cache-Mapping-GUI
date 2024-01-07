5d6309efbf9e30f23959861e0b3afaea153e4991
Muhammad Iyaaz Hakimi <98258588+iyaazhakimi@users.noreply.github.com>
2024-01-08 07:44:25 +0800

    Update gui.py

--- cache mapping build/gui.py
+++ cache mapping build/gui.py
@@ -12,7 +12,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, ttk
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Cache Mapping GUI\cache mapping gui\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Cache Mapping GUI\cache mapping build\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
